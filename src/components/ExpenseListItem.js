e86f206f76fac4db4e3c67d34702da026a499129
Osman Ali <okali@uci.edu>
2018-06-03 07:36:54 -0700

    Fixed import statment

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 import { Link } from 'react-router-dom';
