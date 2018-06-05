1f07e24bd1c5ce9c67921a65f4edc35b19f0a30d
Osman Ali <okali@uci.edu>
2018-06-05 00:27:46 -0700

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -17,7 +17,7 @@ export const startAddExpense = (expenseData = {}) => {
         } = expenseData;
         const expense = { description, note, amount, createdAt };
 
-        database.ref('expenses').push(expense).then((ref) => {
+        return database.ref('expenses').push(expense).then((ref) => {
             dispatch(addExpense({
                 id: ref.key,
                 ...expense
@@ -36,12 +36,18 @@ export const editExpense = (id, edits) => ({
 });
 
 // REMOVE_EXPENSE
-export const removeExpense = ({id} = {}) => ({
+export const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
 
-// export const startRemoveExpense = ()
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).set(null).then(() => {
+            dispatch(removeExpense({ id }));            
+        });
+    };
+};
 
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
