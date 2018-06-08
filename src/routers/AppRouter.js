bb79aa9e75ca529221a34cf0cec5d60a5595813b
1yzo <33081256+1yzo@users.noreply.github.com>
2018-06-07 18:57:06 -0700

    Fixes #2

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -13,15 +13,13 @@ export const history = createHistory();
 
 const AppRouter = (props) => (
     <Router history={history}>
-        <div>
-            <Switch>
-                <PublicRoute path="/" component={LoginPage} exact={true} />
-                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
-                <PrivateRoute path="/create" component={AddExpensePage} />
-                <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
+        <Switch>
+            <PublicRoute path="/" component={LoginPage} exact={true} />
+            <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+            <PrivateRoute path="/create" component={AddExpensePage} />
+            <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+            <Route component={NotFoundPage} />
+        </Switch>
     </Router>
 );
 
